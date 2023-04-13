12360ad05c7f8c4e4f9751e49da275732cb5bb0c
Hiago Peroni <106550392+Hiagoperoni@users.noreply.github.com>
2023-04-13 15:52:06 +0000

    Implementando a Entity do InfoSensor

--- src/InfosSensor/infoSensor.module.ts
+++ src/InfosSensor/infoSensor.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { InfoSensorController } from './infoSensor.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { InfoSensorEntity } from './entity/infoSensor.entity';
 
 @Module({
-    imports: [],
+    imports: [TypeOrmModule.forFeature([InfoSensorEntity])],
     controllers: [InfoSensorController],
     providers: [],
-    exports: [],
+    exports: [TypeOrmModule],
 })
 
 export class InfoSensorModule {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { InfoSensorModule } from './InfosSensor/infoSensor.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { InfoSensorEntity } from './InfosSensor/entity/infoSensor.entity';
+
 
 @Module({
-  imports: [InfoSensorModule],
+  imports: [InfoSensorModule, TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    entities: [InfoSensorEntity],
+    synchronize: process.env.ENV === "development",
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
